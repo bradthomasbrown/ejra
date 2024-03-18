da9583426fb90a73e3fc0ae09677cd095ee91772
Brad Brown <bradbrown@bradbrown.llc>
2024-03-18 18:13:58 -0400

    🐌

--- lib/call.ts
+++ lib/call.ts
@@ -1,4 +1,9 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
+import { Lazy } from 'https://deno.land/x/lazy_promise@0.0.1/mod.ts'
+import { Snail } from 'https://deno.land/x/snail@0.0.0/mod.ts'
+import { ejra } from './mod.ts'
+
+const replacer = (_:unknown,v:unknown)=>typeof v=='bigint'?''+v:v
 
 export async function call<
     E extends {
@@ -16,19 +21,21 @@ export async function call<
     const headers = { 'Content-Type': 'application/json' } as const
     const init = { body, headers, method: 'POST' } as const
 
-    // get response
-    const response = await fetch(url, init)
+    // construct lazy
+    const lazy:Lazy<z.infer<E['schema']>> = async () => {
+        const response = await fetch(url, init)
+        const json = await response.json()
+        const { result } = z.object({ result: z.unknown() }).parse(json)
+        return request.schema.parse(result)
+    }
 
-    // turn response into json
-    const json = await response.json()
-
-    // extract result and error from json
-    const { result } = z.object({
-        result: z.unknown(),
-        // error: z.object({ message: z.string() }).optional()
-    }).parse(json)
-
-    // parse the result with the given schema
-    return request.schema.parse(result)
+    // 🐌
+    const snail = new Snail(lazy)
+    snail.born
+        .then(() => ejra.out.push(`ejra.call sending request ${JSON.stringify(request, replacer)}`))
+    snail.died
+        .then(value => ejra.out.push(`ejra.call received value ${JSON.stringify(value, replacer)} for request ${JSON.stringify(request, replacer)}`))
+        .catch(reason => ejra.err.push(new Error(`ejra.call failed requesting ${JSON.stringify(request, replacer)}`, { cause: reason })))
+    return snail.lazy().catch(() => {})
 
 }
