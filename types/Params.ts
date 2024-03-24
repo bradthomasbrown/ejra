b5dc73c57c91eef06d336d0b18ad177576e93f77
Brad Brown <bradbrown@bradbrown.llc>
2024-03-24 17:30:55 -0400

    add tag to method call

--- types/Params.ts
+++ types/Params.ts
@@ -11,7 +11,7 @@ export type Params = {
     nonce: [address:string, tag:Tag],
     code: [address:string, tag:Tag],
     send: [data:string],
-    call: [txCallObject:TxCallObject]
+    call: [txCallObject:TxCallObject, tag:Tag]
     estimateGas: [txCallObject:Partial<TxCallObject>],
     receipt: [hash:string]
     logs: [filter:Filter]
