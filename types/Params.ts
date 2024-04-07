b14123e832d102551ca8c071116c768b36a902f3
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 22:26:49 -0400

    add options to params

--- types/Params.ts
+++ types/Params.ts
@@ -1,18 +1,18 @@
 import { Filter, Tag, TxCallObject } from './mod.ts'
 
 export type Params = {
-    clientVersion: [],
-    sha3: [data:string],
-    chainId: [],
-    gasPrice: [],
-    height: [],
-    balance: [address:string, tag:Tag],
-    slot: [address:string, slot:bigint, tag:Tag],
-    nonce: [address:string, tag:Tag],
-    code: [address:string, tag:Tag],
-    send: [data:string],
-    call: [txCallObject:TxCallObject, tag:Tag]
-    estimateGas: [txCallObject:Partial<TxCallObject>],
-    receipt: [hash:string]
-    logs: [filter:Filter]
+    clientVersion: [options?:{ signal:AbortSignal }],
+    sha3: [data:string, options?:{ signal:AbortSignal }],
+    chainId: [options?:{ signal:AbortSignal }],
+    gasPrice: [options?:{ signal:AbortSignal }],
+    height: [options?:{ signal:AbortSignal }],
+    balance: [address:string, tag:Tag, options?:{ signal:AbortSignal }],
+    slot: [address:string, slot:bigint, tag:Tag, options?:{ signal:AbortSignal }],
+    nonce: [address:string, tag:Tag, options?:{ signal:AbortSignal }],
+    code: [address:string, tag:Tag, options?:{ signal:AbortSignal }],
+    send: [data:string, options?:{ signal:AbortSignal }],
+    call: [txCallObject:TxCallObject, tag:Tag, options?:{ signal:AbortSignal }]
+    estimateGas: [txCallObject:Partial<TxCallObject>, options?:{ signal:AbortSignal }],
+    receipt: [hash:string, options?:{ signal:AbortSignal }]
+    logs: [filter:Filter, options?:{ signal:AbortSignal }]
 }
