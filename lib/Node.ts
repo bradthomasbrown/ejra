10eb79f0bc3e98794e168769303e2119d86f795f
Brad Brown <bradbrown@bradbrown.llc>
2024-05-19 22:15:26 -0400

    update node object with new methods

--- lib/Node.ts
+++ lib/Node.ts
@@ -4,21 +4,25 @@ import { Tag } from './types/Tag.ts';
 
 export class Node {
     rpc:string
+    balance:(address:string, tag:Tag)=>ReturnType<typeof methods.balance>
     call:(txCallObject:TxCallObject, tag:Tag)=>ReturnType<typeof methods.call>
     chainId:()=>ReturnType<typeof methods.chainId>
     clientVersion:()=>ReturnType<typeof methods.clientVersion>
     estimateGas:(txCallObject:Partial<TxCallObject>, tag:Tag)=>ReturnType<typeof methods.estimateGas>
     gasPrice:()=>ReturnType<typeof methods.gasPrice>
     height:()=>ReturnType<typeof methods.height>
+    receipt:(hash:string)=>ReturnType<typeof methods.receipt>
     sendRawTx:(data:string)=>ReturnType<typeof methods.sendRawTx>
     constructor(rpc:string) {
         this.rpc = rpc
+        this.balance = (address:string, tag:Tag) => methods.balance(rpc, address, tag)
         this.call = (txCallObject:TxCallObject, tag:Tag) => methods.call(rpc, txCallObject, tag)
         this.chainId = () => methods.chainId(rpc)
         this.clientVersion = () => methods.clientVersion(rpc)
         this.estimateGas = (txCallObject:Partial<TxCallObject>, tag:Tag) => methods.estimateGas(rpc, txCallObject, tag)
         this.gasPrice = () => methods.gasPrice(rpc)
         this.height = () => methods.height(rpc)
+        this.receipt = (hash:string) => methods.receipt(rpc, hash)
         this.sendRawTx = (data:string) => methods.sendRawTx(rpc, data)
     }
 }
