8d3baaf83c93a45c64e3f4cc789e75196c0e29e5
Brad Brown <bradbrown@bradbrown.llc>
2024-05-30 09:18:53 -0400

    add traceTx method

---
+++ lib/methods/traceTx.ts
@@ -0,0 +1,24 @@
+import * as jra from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/jra@0.1.1/mod.ts'
+import * as schemas from "../schemas/mod.ts";
+
+type TraceConfig = {
+    enableMemory:boolean
+    disableStack:boolean
+    disableStorage:boolean
+    enableReturnData:boolean
+}
+
+export async function traceTx(rpc: string, hash:string) {
+  const method = "debug_traceTransaction";
+  const params = [
+    hash,
+    {
+        enableMemory: true,
+        disableStack: false,
+        disableStorage: false,
+        enableReturnData: true
+    }
+  ]
+  const schema = schemas.string;
+  return schema.parse(await new jra.Client(rpc).request(method, params, 0))
+}
