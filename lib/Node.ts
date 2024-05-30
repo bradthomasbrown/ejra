5171cfcd8ee8b6cb97a550b97f713ae82c42dcfa
Brad Brown <bradbrown@bradbrown.llc>
2024-05-29 20:52:24 -0400

    add methods code and slot to Node class

--- lib/Node.ts
+++ lib/Node.ts
@@ -4,7 +4,7 @@ import { Tag } from './types/Tag.ts';
 
 export class Node {
     rpc:string
-    balance:(address:string, tag:Tag)=>ReturnType<typeof methods.balance>
+    balance:(address:string,tag:Tag)=>ReturnType<typeof methods.balance>
     call:(txCallObject:TxCallObject, tag:Tag)=>ReturnType<typeof methods.call>
     chainId:()=>ReturnType<typeof methods.chainId>
     clientVersion:()=>ReturnType<typeof methods.clientVersion>
@@ -13,6 +13,8 @@ export class Node {
     height:()=>ReturnType<typeof methods.height>
     receipt:(hash:string)=>ReturnType<typeof methods.receipt>
     sendRawTx:(data:string)=>ReturnType<typeof methods.sendRawTx>
+    slot:(address:string,slot:bigint,tag:Tag)=>ReturnType<typeof methods.slot>
+    code:(address:string,tag:Tag)=>ReturnType<typeof methods.code>
     constructor(rpc:string) {
         this.rpc = rpc
         this.balance = (address:string, tag:Tag) => methods.balance(rpc, address, tag)
@@ -24,5 +26,7 @@ export class Node {
         this.height = () => methods.height(rpc)
         this.receipt = (hash:string) => methods.receipt(rpc, hash)
         this.sendRawTx = (data:string) => methods.sendRawTx(rpc, data)
+        this.slot = (address:string, slot:bigint, tag:Tag) => methods.slot(rpc, address, slot, tag)
+        this.code = (address:string, tag:Tag) => methods.code(rpc, address, tag)
     }
 }
